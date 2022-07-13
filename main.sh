5554da4c0a2a3c394b0b68d22d83b8434eec41d8
Alan Yanse <yansealan@gmail.com>
2022-07-13 16:10:30 -0300

    Creación de la presentación y el rótulo y un poco del menú principal

---
+++ main.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#importando el Plugin para generar un arrow key
+source plugs/select-arrow.sh
+
+#Creando el menú principal
+
+cat assets/banner.txt
+
+echo ""
+echo "    Alumno : Alan Yanse"
+echo "Asignatura : Entorno de la Programación"
+echo "             TUIA UNR 2022"
+echo ""
+
+
+# Validación argumentos ... cantidad de argumentos permitidos = 1, el cual debe ser el archivo de texto para analizar
+
+
+# Crear una sub-presentación en la cual dice algo así como: "El archivo que se va a analizar es ..."
+
+
+options=("Yes" "No" "Cancel" "${array[@]}") # join arrays to add some variable array
+
+case `select_opt "${options[@]}"` in
+    0) OPTION="selected Yes";;
+    1) OPTION="selected No";;
+    *) OPTION="selected ${options[$?]}";;
+esac
+
+
+
+echo $OPTION
+
+
+
+
