1861ec62822efa267942ec60d1aab8004f50f328
Alan Yanse <yansealan@gmail.com>
2022-07-21 16:09:11 -0300

    comenzando con el primer ejercicio

--- scripts-childs/statsWords.sh
+++ scripts-childs/statsWords.sh
@@ -3,3 +3,37 @@
 
 # Hallar en un archivo , la palabra mas larga, la mas corta y el promedio de longitud
 # statsWords
+
+
+# ----- ZONA DE FUNCIONES -----
+
+function cant_words(){
+
+    # Obtiene la cantidad de palabras de un archivo
+
+    wc -w $1 | awk '{print $1}'
+}
+
+WORDS=`cant_words $1`
+
+
+echo "la cantidad de palabras que tiene el archivo es: $WORDS"
+
+
+function cant_characters(){
+
+    # Obtiene la cantidad de caracteres de un archivo
+
+    wc -m $1 | awk '{print $1}'
+}
+
+
+CHARACTERS=`cant_characters $1`
+
+
+echo "la cantidad de caracteres que tiene el archivo es: $CHARACTERS"
+
+
+
+
+
